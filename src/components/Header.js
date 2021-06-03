72650efaff0e644886a7d3cafacb2c829ee5dfb2
Vanessa720 <vanessawilliamson718@gmail.com>
2021-06-03 16:43:43 -0400

    Completed Assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+
 import React from "react";
 import Search from "./Search";
 
-function Header() {
+function Header({ onSearch }) {
   return (
     <header>
       <h1>
@@ -10,7 +11,7 @@ function Header() {
         </span>
         gregslist
       </h1>
-      <Search />
+      <Search onSearch={onSearch} />
     </header>
   );
 }
